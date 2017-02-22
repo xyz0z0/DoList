4871a3a591c9f85ed53a9ad8052acb09fbfb3827
xyz0z0 <xyz0z0.xyz@gmail.com>
2017-02-22 21:55:42 +0800

    commit

---
+++ library/src/main/java/com/yalantis/beamazingtoday/interfaces/BatModel.java
@@ -0,0 +1,16 @@
+package com.yalantis.beamazingtoday.interfaces;
+
+/**
+ * Created by galata on 22.08.16.
+ */
+public interface BatModel {
+
+    String getId();
+
+    boolean isChecked();
+
+    String getText();
+
+    void setChecked(boolean checked);
+
+}
